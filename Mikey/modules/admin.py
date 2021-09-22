c1e4a54c168af07e7c271a0051706c1a8be0ce13
CraZyGuy456 <75108109+CraZyGuy456@users.noreply.github.com>
2021-09-22 22:22:17 +0530

    admin

--- Mikey/modules/admin.py
+++ Mikey/modules/admin.py
@@ -483,7 +483,7 @@ dispatcher.add_handler(DEMOTE_HANDLER)
 dispatcher.add_handler(SET_TITLE_HANDLER)
 dispatcher.add_handler(ADMIN_REFRESH_HANDLER)
 
-__mod_name__ = "Admeme"
+__mod_name__ = "Admin"
 __command_list__ = [
     "adminlist", "admins", "invitelink", "promote", "demote", "admincache"
 ]
