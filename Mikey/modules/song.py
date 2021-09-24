06b1be248d00a4a85e0272777e819b4264b0791a
CraZyGuy456 <75108109+CraZyGuy456@users.noreply.github.com>
2021-09-24 19:27:24 +0530

    Update song.py

--- Mikey/modules/song.py
+++ Mikey/modules/song.py
@@ -1,7 +1,7 @@
 import asyncio, time, os, asyncio, json
 from telethon.tl.types import DocumentAttributeAudio
 from Mikey.events import register
-from Mikey.utlis import progress
+from Mikey.utils import progress
 from youtube_dl import YoutubeDL
 from youtube_dl.utils import (DownloadError, ContentTooShortError,
 
