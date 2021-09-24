1785ada5e6d65d9a4c1b51f5330cf03533ecc7fe
CraZyGuy456 <75108109+CraZyGuy456@users.noreply.github.com>
2021-09-24 17:19:21 +0530

    create

---
+++ Mikey/modules/song.py
@@ -0,0 +1,318 @@
+import asyncio, time, os, asyncio, json
+from telethon.tl.types import DocumentAttributeAudio
+from Mikey.events import register
+from Mikey.utlis import progress
+from youtube_dl import YoutubeDL
+from youtube_dl.utils import (DownloadError, ContentTooShortError,
+
+                              ExtractorError, GeoRestrictedError,
+                              MaxDownloadsReached, PostProcessingError,
+                              UnavailableVideoError, XAttrMetadataError)
+try:
+   from youtubesearchpython import SearchVideos 
+
+except:
+	os.system("pip install pip install youtube-search-python")
+	from youtubesearchpython import SearchVideos 
+	pass
+
+#@register(pattern="^/song (.*)")
+async def download_video(v_url):
+
+    lazy = v_url ; sender = await lazy.get_sender() ; me = await lazy.client.get_me()
+
+    if not sender.id == me.id:
+        rkp = await lazy.reply("`processing...`")
+    else:
+    	rkp = await lazy.edit("`processing...`")   
+    url = v_url.pattern_match.group(1)
+    if not url:
+         return await rkp.edit("`Error \nusage song <song name>`")
+    search = SearchVideos(url, offset = 1, mode = "json", max_results = 1)
+    test = search.result()
+    p = json.loads(test)
+    q = p.get('search_result')
+    try:
+       url = q[0]['link']
+    except:
+    	return await rkp.edit("`failed to find`")
+    type = "audio"
+    await rkp.edit("`Preparing to download...`")
+    if type == "audio":
+        opts = {
+            'format':
+            'bestaudio',
+            'addmetadata':
+            True,
+            'key':
+            'FFmpegMetadata',
+            'writethumbnail':
+            True,
+            'prefer_ffmpeg':
+            True,
+            'geo_bypass':
+            True,
+            'nocheckcertificate':
+            True,
+            'postprocessors': [{
+                'key': 'FFmpegExtractAudio',
+                'preferredcodec': 'mp3',
+                'preferredquality': '320',
+            }],
+            'outtmpl':
+            '%(id)s.mp3',
+            'quiet':
+            True,
+            'logtostderr':
+            False
+        }
+        video = False
+        song = True    
+    try:
+        await rkp.edit("`Fetching data, please wait..`")
+        with YoutubeDL(opts) as rip:
+            rip_data = rip.extract_info(url)
+    except DownloadError as DE:
+        await rkp.edit(f"`{str(DE)}`")
+        return
+    except ContentTooShortError:
+        await rkp.edit("`The download content was too short.`")
+        return
+    except GeoRestrictedError:
+        await rkp.edit(
+            "`Video is not available from your geographic location due to geographic restrictions imposed by a website.`"
+        )
+        return
+    except MaxDownloadsReached:
+        await rkp.edit("`Max-downloads limit has been reached.`")
+        return
+    except PostProcessingError:
+        await rkp.edit("`There was an error during post processing.`")
+        return
+    except UnavailableVideoError:
+        await rkp.edit("`Media is not available in the requested format.`")
+        return
+    except XAttrMetadataError as XAME:
+        await rkp.edit(f"`{XAME.code}: {XAME.msg}\n{XAME.reason}`")
+        return
+    except ExtractorError:
+        await rkp.edit("`There was an error during info extraction.`")
+        return
+    except Exception as e:
+        await rkp.edit(f"{str(type(e)): {str(e)}}")
+        return
+    c_time = time.time()
+    if song:
+        await rkp.edit(f"`Preparing to upload song:`\
+        \n**{rip_data['title']}**\
+        \nby *{rip_data['uploader']}*")
+        await v_url.client.send_file(
+            v_url.chat_id,
+            f"{rip_data['id']}.mp3",
+            supports_streaming=True,
+            attributes=[
+                DocumentAttributeAudio(duration=int(rip_data['duration']),
+                                       title=str(rip_data['title']),
+                                       performer=str(rip_data['uploader']))
+            ],
+            progress_callback=lambda d, t: asyncio.get_event_loop(
+            ).create_task(
+                progress(d, t, v_url, c_time, "Uploading..",
+                         f"{rip_data['title']}.mp3")))
+        os.remove(f"{rip_data['id']}.mp3")
+    elif video:
+        await rkp.edit(f"`Preparing to upload song :`\
+        \n**{rip_data['title']}**\
+        \nby *{rip_data['uploader']}*")
+        await v_url.client.send_file(
+            v_url.chat_id,
+            f"{rip_data['id']}.mp4",
+            supports_streaming=True,
+            caption=url,
+            progress_callback=lambda d, t: asyncio.get_event_loop(
+            ).create_task(
+                progress(d, t, v_url, c_time, "Uploading..",
+                         f"{rip_data['title']}.mp4")))
+        os.remove(f"{rip_data['id']}.mp4")
+
+
+@register(pattern="^/video (.*)")
+async def download_video(v_url):  
+    lazy = v_url ; sender = await lazy.get_sender() ; me = await lazy.client.get_me()
+    if not sender.id == me.id:
+        rkp = await lazy.reply("`processing...`")
+    else:
+    	rkp = await lazy.edit("`processing...`")   
+    url = v_url.pattern_match.group(1)
+    if not url:
+         return await rkp.edit("`Error \nusage song <song name>`")
+    search = SearchVideos(url, offset = 1, mode = "json", max_results = 1)
+    test = search.result()
+    p = json.loads(test)
+    q = p.get('search_result')
+    try:
+       url = q[0]['link']
+    except:
+    	return await rkp.edit("`failed to find`")
+    type = "audio"
+    await rkp.edit("`Preparing to download...`")
+    if type == "audio":
+        opts = {
+            'format':
+            'best',
+            'addmetadata':
+            True,
+            'key':
+            'FFmpegMetadata',
+            'prefer_ffmpeg':
+            True,
+            'geo_bypass':
+            True,
+            'nocheckcertificate':
+            True,
+            'postprocessors': [{
+                'key': 'FFmpegVideoConvertor',
+                'preferedformat': 'mp4'
+            }],
+            'outtmpl':
+            '%(id)s.mp4',
+            'logtostderr':
+            False,
+            'quiet':
+            True
+        }
+        song = False
+        video = True
+    try:
+        await rkp.edit("`Fetching data, please wait..`")
+        with YoutubeDL(opts) as rip:
+            rip_data = rip.extract_info(url)
+    except DownloadError as DE:
+        await rkp.edit(f"`{str(DE)}`")
+        return
+    except ContentTooShortError:
+        await rkp.edit("`The download content was too short.`")
+        return
+    except GeoRestrictedError:
+        await rkp.edit(
+            "`Video is not available from your geographic location due to geographic restrictions imposed by a website.`"
+        )
+        return
+    except MaxDownloadsReached:
+        await rkp.edit("`Max-downloads limit has been reached.`")
+        return
+    except PostProcessingError:
+        await rkp.edit("`There was an error during post processing.`")
+        return
+    except UnavailableVideoError:
+        await rkp.edit("`Media is not available in the requested format.`")
+        return
+    except XAttrMetadataError as XAME:
+        await rkp.edit(f"`{XAME.code}: {XAME.msg}\n{XAME.reason}`")
+        return
+    except ExtractorError:
+        await rkp.edit("`There was an error during info extraction.`")
+        return
+    except Exception as e:
+        await rkp.edit(f"{str(type(e)): {str(e)}}")
+        return
+    c_time = time.time()
+    if song:
+        await rkp.edit(f"`Preparing to upload song `\
+        \n**{rip_data['title']}**\
+        \nby *{rip_data['uploader']}*")
+        await v_url.client.send_file(
+            v_url.chat_id,
+            f"{rip_data['id']}.mp3",
+            supports_streaming=True,
+            attributes=[
+                DocumentAttributeAudio(duration=int(rip_data['duration']),
+                                       title=str(rip_data['title']),
+                                       performer=str(rip_data['uploader']))
+            ],
+            progress_callback=lambda d, t: asyncio.get_event_loop(
+            ).create_task(
+                progress(d, t, v_url, c_time, "Uploading..",
+                         f"{rip_data['title']}.mp3")))
+        os.remove(f"{rip_data['id']}.mp3")
+        await v_url.delete()
+    elif video:
+        await rkp.edit(f"`Preparing to upload video song :`\
+        \n**{rip_data['title']}**\
+        \nby *{rip_data['uploader']}*")
+        await v_url.client.send_file(
+            v_url.chat_id,
+            f"{rip_data['id']}.mp4",
+            supports_streaming=True,
+            caption=rip_data['title'],
+            progress_callback=lambda d, t: asyncio.get_event_loop(
+            ).create_task(
+                progress(d, t, v_url, c_time, "Uploading..",
+                         f"{rip_data['title']}.mp4")))
+        os.remove(f"{rip_data['id']}.mp4")
+        await rkp.delete()
+
+__help__ = """
+ *You can either enter just the song name or both the artist and song
+  name. *
+ - `/song` <songname artist(optional)>: uploads the song in it's best quality available
+ - `/video` <songname artist(optional)>: uploads the video song in it's best quality available
+ - `/lyrics` <song>: returns the lyrics of that song.
+I can now also play music in your group's voice chat
+ 
+Assistant username:- @MikeyVCBot
+ 
+ Setting up 
+1) Make bot admin
+2) Start a voice chat
+3) Try `/play [song name]` for the first time by an admin
+) If userbot joined enjoy music, If not add @MikeyVCBot to your group manually and retry
+ Commands:
+ 
+• Song Playing 🎶
+ - `/play` <songname>: Play song using youtube music
+ - `/play` <yt url> : Play the given yt url
+ - `/dplay`: Play song via deezer
+ - `/splay`: Play song via jio saavn
+  
+• Playback ⏯
+ - `/player`: Open Settings menu of player
+ - `/skip`: Skips the current track
+ - `/pause`: Pause track
+ - `/resume`: Resumes the paused track
+ - `/end`: Stops media playback
+ - `/current`: Shows the current Playing track
+ - `/playlist`: Shows playlist
+
+• More tools 📲
+ - `/admincache`: Updates admin info of your group. Try if bot isn't recognize admin
+ - `/userbotjoin`: Invite @EsDeathVCbot to your chat
+
+• Channel Music Play 🛠
+1) Make me admin of your channel 
+2) Send /userbotjoinchannel in linked group
+3) Now send commands in linked group
+
+🏷 For linked group admins only:
+- /cplay [song name] - play song you requested
+- /cdplay [song name] - play song you requested via deezer
+- /csplay [song name] - play song you requested via jio saavn
+- /cplaylist - Show now playing list
+- /cccurrent - Show now playing
+- /cplayer - open music player settings panel
+- /cpause - pause song play
+- /cresume - resume song play
+- /cskip - play next song
+- /cend - stop music play
+- /userbotjoinchannel - invite assistant to your chat
+channel is also can be used instead of c ( /cplay = /channelplay )
+
+🏷 If you don't like to play in linked group:
+1) Get your channel ID.
+2) Create a group with tittle: Channel Music: <your_channel_id>
+3) Add bot as Channel admin with full perms
+4) Add @EsDeathVCbot to the channel as an admin.
+5) Simply send commands in your group.
+"""
+
+__mod_name__ = "Music"
